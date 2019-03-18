66c7695be09f91b9a831b58484e190d7b5bc1de9
James Kusmambang <kusmambang@gmail.com>
2019-03-18 13:38:05 +0700

    test rest

--- src/main/java/com/paralun/app/SpringBootRestApplication.java
+++ src/main/java/com/paralun/app/SpringBootRestApplication.java
@@ -2,8 +2,10 @@ package com.paralun.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootRestApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/paralun/app/model/Barang.java
+++ src/main/java/com/paralun/app/model/Barang.java
@@ -3,6 +3,7 @@ package com.paralun.app.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,25 +11,32 @@ import java.util.Date;
 
 @Entity
 @Table(name = "barang")
+@EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
 public class Barang implements Serializable {
 
     @Id
     @Column(name = "kode")
     private String kode;
+
     @Column(name = "nama_barang", nullable = false)
     private String namaBarang;
+
     @Column(name = "kategori")
     private String kategori;
+
     @Column(name = "stok")
     private Integer stok;
+
     @Column(name = "harga")
     private Long harga;
-    @Column(name = "createdAt", nullable = false, updatable = false)
+
+    @Column(nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
     private Date createdAt;
-    @Column(name = "updatedAt", nullable = false)
+
+    @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
